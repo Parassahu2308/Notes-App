a2a2a4d8a1c0da9b7f396db57d1de740921b59b8
Parassahu2308 <sahujuparas786@gmail.com>
2022-02-27 11:47:05 +0530

    updated

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hello");
+// if user add a note, add in the local storage
+
+let addbtn = document.getElementById('addbtn');
+addbtn.addEventListener('click', function (e){
+     
+    let addtxt = document.getElementById('addtxt');
+    let notes = localStorage.getItem('notes');
+    if(notes == null){
+        notesObj = [];
+    }
+    else{
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.push(addtxt.value);
+    localStorage.setItem('notes', JSON.stringify(notesObj));
+    addtxt.value=" ";
+    console .log(notesObj);
+})
